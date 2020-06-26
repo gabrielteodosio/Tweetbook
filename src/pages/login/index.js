73d2d04566025860948e0af855b08a14e42c3d99
Gabriel T Marques <gabrieltmm@gmail.com>
2020-06-26 01:07:17 -0300

    firsts icons added

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import { MdLockOpen, FaUserCircle } from 'react-icons/all'
 
 import styles from './login.module.less'
+import colors from '../../theme/colors'
 
 function LoginPage () {
   return (
@@ -8,7 +10,24 @@ function LoginPage () {
       <div className={styles.card}>
         <div className={styles.left}>
           <h1>Tweetbook</h1>
+
+          <div className={styles.iconsContainer}>
+            <div className={styles.lockContainer}>
+              <button>
+                <MdLockOpen size={36} color={colors.white} />
+                No protection
+              </button>
+            </div>
+
+            <div className={styles.profileContainer}>
+              <button>
+                <FaUserCircle size={36} color={colors.white} />
+                No on to chat
+              </button>
+            </div>
+          </div>
         </div>
+
         <div className={styles.right} />
       </div>
     </div>
